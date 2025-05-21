b3e37857ffd48f4734b71a13b9600e633bbadace
jur0ss <jurekgz27@gmail.com>
2025-05-21 12:53:09 +0200

    Working on Submit and AddCity buttons

--- TravelPlanner/MainWindow.axaml.cs
+++ TravelPlanner/MainWindow.axaml.cs
@@ -1,13 +1,33 @@
+using System.Collections.ObjectModel;
 using Avalonia.Controls;
 
 namespace TravelPlanner;
 
 public partial class MainWindow : Window
 {
+    private ObservableCollection<string> _cities = new();
+
     public MainWindow()
     {
         InitializeComponent();
-        
 
+        CityListBox.ItemsSource = _cities; 
+        AddCityButton.Click += AddCityButton_Click;
+        SubmitButton.Click += SubmitButton_Click;
+    }
+
+    private void AddCityButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var city = CityTextBox.Text?.Trim();
+        if (!string.IsNullOrWhiteSpace(city))
+        {
+            _cities.Add(city);
+            CityTextBox.Text = "";
+        }
+    }
+
+    private void SubmitButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        
     }
 }
--- TravelPlanner/obj/Debug/net9.0/TravelPlanner.AssemblyInfo.cs
+++ TravelPlanner/obj/Debug/net9.0/TravelPlanner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelPlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54dbc4ec199caa7d072b228144bc302451b3860e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da24bf488ffbb1eddfaad83df13b4c15d073b649")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelPlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelPlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
